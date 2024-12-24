bb1b794539ff1b0ef37db2f03fd43d8e95809070
Maralov Kemal <37023297+pikopiko15@users.noreply.github.com>
2024-12-24 16:17:59 +0500

    added a razor page (#1)
    Co-authored-by: Kemal Maralov <Kemal_Maralov@epam.com>

---
+++ Backend/Pages/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Backend.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
